93231afaf5d5efb0c4854f0167faedcf9bb6a57d
WhoopyEv <WhoopyEv@gmail.com>
2023-12-29 14:01:24 -0500

    Se separa el algoritmo de la interfaz

--- algoritmo.py
+++ algoritmo.py
@@ -1,8 +1,20 @@
-def ordenamiento(arr: list):
+import flet as ft
+import time
+
+
+def ordenamiento(arr: list, page: ft.Page):
     n = len(arr)
     for iteracion in range(n):
         for posicion in range(0, n - iteracion - 1):
-            if arr[posicion] > arr[posicion + 1]:
+
+            arr[posicion].bgcolor = ft.colors.YELLOW
+            arr[posicion + 1].bgcolor = ft.colors.YELLOW
+            time.sleep(0.5)
+            page.update()
+            if int(arr[posicion].content.value) > int(arr[posicion + 1].content.value):
                 arr[posicion], arr[posicion + 1] = arr[posicion + 1], arr[posicion]
 
-
+            arr[posicion].bgcolor = ft.colors.ORANGE
+            arr[posicion + 1].bgcolor = ft.colors.ORANGE
+        arr[n - iteracion - 1].bgcolor = ft.colors.GREEN
+    page.update()
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import flet as ft
 import random
-import time
 from algoritmo import ordenamiento
 
 
@@ -27,23 +26,7 @@ def main(page: ft.Page):
 
     page.add(row)
 
-    arr = row.controls
-    n = len(arr)
-    for iteracion in range(n):
-        for posicion in range(0, n - iteracion - 1):
-
-            arr[posicion].bgcolor = ft.colors.YELLOW
-            arr[posicion + 1].bgcolor = ft.colors.YELLOW
-            time.sleep(0.5)
-            page.update()
-            if int(arr[posicion].content.value) > int(arr[posicion + 1].content.value):
-                arr[posicion], arr[posicion + 1] = arr[posicion + 1], arr[posicion]
-
-            arr[posicion].bgcolor = ft.colors.ORANGE
-            arr[posicion + 1].bgcolor = ft.colors.ORANGE
-        arr[n - iteracion - 1].bgcolor = ft.colors.GREEN
-    page.update()
-    print(arr)
+    ordenamiento(row.controls, page)
 
 
 ft.app(target=main)
